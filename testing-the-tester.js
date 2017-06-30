1f7de5ca61977cc1b37d71203d6768f69c462979
NeoMH91 <NeoMengHwee1991@hotmail.com>
2017-06-30 16:17:21 +0800

    Tic Tac Toe

---
+++ testing-the-tester.js
@@ -0,0 +1,17 @@
+var restart = function() {
+
+}
+
+var isGameOver = function() {
+ return false
+}
+
+var whoWon = function() {
+  var int = 0
+  return int
+}
+
+var playTurn = function() {
+  var move = 0
+
+}
